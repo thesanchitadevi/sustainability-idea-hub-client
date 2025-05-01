ad09b3d198406241bffb378f6c0db497f1420945
Sanchita Devi <thesanchitadevi@gmail.com>
2025-05-01 18:07:31 +0600

    Add new components and update configuration; implement HeroSection, AboutPage, BlogPage, AdminDashboard, MemberDashboard, and LoginPage. Enhance global styles with custom themes and integrate new dependencies for improved functionality.

--- src/app/(CommonLayout)/page.tsx
+++ src/app/(CommonLayout)/page.tsx
@@ -1,12 +1,15 @@
+import HeroSection from "@/components/ui/modules/Home/Hero";
+
 const HomePage = async () => {
-    // Data fetching
-    // const res = await fetch("http://localhost:5000/products")
-  
-    return (
-      <div>
-        Home Page
-      </div>
-    );
-  };
-  
-  export default HomePage;
+  // Data fetching
+  // const res = await fetch("http://localhost:5000/products")
+
+  return (
+    <div>
+      Home Page
+      <HeroSection />
+    </div>
+  );
+};
+
+export default HomePage;
